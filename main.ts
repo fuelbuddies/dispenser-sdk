0e629c77902c94357da4d9318d6c2dffe1e40abc
Nitin Sharma <nitin@fuelbuddy.in>
2024-03-30 14:40:27 +0400

    NO_T fixing release
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- main.ts
+++ main.ts
@@ -1,13 +1,8 @@
-import { Tokhiem } from "./dispenser/Tokhiem";
-import { IsoilVegaTVersion10 } from "./dispenser/IsoilVegaTVersion10";
-import { GateX } from "./dispenser/GateX";
-import { RfidPetropoint } from "./rfid/RfidPetropoint";
 import { findDispenserPort } from "./utils/findDispenserPort";
 import { findRfidPort } from "./utils/findRfidPort";
 import { DispenserOptions, IDispenser } from "./dispenser/interface/IDispenser";
 import { SerialPort } from "serialport";
 import { IRfid, RfidOptions } from "./rfid/interface/IRfid";
-import ModbusRTU from "modbus-serial";
 
 export { IDispenser, IRfid, RfidOptions, DispenserOptions};
 
@@ -16,13 +11,17 @@ export async function createDispenser(options: DispenserOptions): Promise<IDispe
     const { dispenserType, hardwareId, attributeId, baudRate = 9600 } = options;
     switch (dispenserType) {
         case 'Tokhiem':
-            return new Tokhiem(new SerialPort({path: await findDispenserPort(hardwareId, attributeId), baudRate: baudRate }), options);
+            const DispenserType = await import('./dispenser/Tokhiem');
+            return new DispenserType.Tokhiem(new SerialPort({path: await findDispenserPort(hardwareId, attributeId), baudRate: baudRate }), options);
         case 'IsoilVegaTVersion10':
-            return new IsoilVegaTVersion10(new SerialPort({path: await findDispenserPort(hardwareId, attributeId), baudRate: baudRate }), options);
+            const IsoilVegaTVersion10 = await import('./dispenser/IsoilVegaTVersion10');
+            return new IsoilVegaTVersion10.IsoilVegaTVersion10(new SerialPort({path: await findDispenserPort(hardwareId, attributeId), baudRate: baudRate }), options);
         case 'GateX':
-            const serialPort = new ModbusRTU();
+            const GateX = await import('./dispenser/GateX');
+            const ModbusRTU = await import('modbus-serial');
+            const serialPort = new ModbusRTU.default();
             await serialPort.connectRTUBuffered(await findDispenserPort(hardwareId, attributeId), { baudRate: baudRate });
-            return new GateX(serialPort, options);
+            return new GateX.GateX(serialPort, options);
         default:
             throw new Error('Invalid dispenser type');
     }
@@ -31,10 +30,11 @@ export async function createDispenser(options: DispenserOptions): Promise<IDispe
 /* Factory for creating dispenser objects */
 export async function createRfid(options: RfidOptions): Promise<IRfid> {
     const { rfidType, hardwareId, attributeId, baudRate = 19200 } = options;
-    const serialPort = new SerialPort({path: await findRfidPort(hardwareId, attributeId), baudRate: baudRate });
     switch (rfidType) {
         case 'PETROPOINTHECTRONICS':
-            return new RfidPetropoint(serialPort);
+            const serialPort = new SerialPort({path: await findRfidPort(hardwareId, attributeId), baudRate: baudRate });
+            const RfidPetropoint = await import('./rfid/RfidPetropoint');
+            return new RfidPetropoint.RfidPetropoint(serialPort);
         default:
             throw new Error('Invalid dispenser type');
     }
