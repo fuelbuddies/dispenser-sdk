5bc30bff1dd0bdb41a1697c538393af48845eb79
Nitin Sharma <nitin@fuelbuddy.in>
2024-04-30 17:25:25 +0400

    NO_T fixing release.
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

---
+++ .bin/pump-stop.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env ts-node
+
+import { createDispenser } from '../main';
+import {getConfigFromEnv} from '../utils/envParser';
+
+console.log(getConfigFromEnv());
+
+createDispenser(getConfigFromEnv()).then((dispenser) => {
+    // console.log(dispenser);
+    dispenser.execute(dispenser.pumpStop, dispenser.processCommand).then((totalizer) => {
+        dispenser.disconnect(() => {
+            console.log("Disconnected");
+        });
+        console.log(totalizer);
+    });
+});
