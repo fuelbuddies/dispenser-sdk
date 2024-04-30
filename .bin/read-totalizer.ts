1404e0bba7604c7c8fb222dcaebf0963c4b07456
Nitin Sharma <nitin@fuelbuddy.in>
2024-04-30 15:30:39 +0400

    NO_T fixin bug in async
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- .bin/read-totalizer.ts
+++ .bin/read-totalizer.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env ts-node
 
 import { createDispenser } from '../main';
-import {getConfigFromEnv} from '../utils/envParser';
+import { getConfigFromEnv } from '../utils/envParser';
 
 async function main() {
     const dispenser = await createDispenser(getConfigFromEnv());
     const totalizer = await dispenser.execute(dispenser.totalizer, dispenser.processTotalizer)
-    console.log(totalizer);
+    return totalizer;
 }
 
-main();
+console.log(main());
