a8e1ef4e15a313fff407635ec3f9f87281dad6ed
Nitin Sharma <nitin@fuelbuddy.in>
2024-05-04 17:12:23 +0400

    NO_T fixing release.
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- .bin/print.ts
+++ .bin/print.ts
@@ -7,7 +7,27 @@ console.log(getConfigFromEnv());
 
 createDispenser(getConfigFromEnv()).then((dispenser) => {
     // console.log(dispenser);
-    dispenser.execute(dispenser.printReceipt, dispenser.processCommand).then((totalizer) => {
+    const printObj = {
+        "customerCode": null,
+        "driverCode": 7777,
+        "endBatchCode": null,
+        "endTime": "2024-05-04T08:16:00.194462",
+        "endTotalizer": "246",
+        "orderCode": 113175,
+        "productName": "Diesel",
+        "orderDate": "2024-04-26T01:00:00",
+        "quantity": 123,
+        "startBatchCode": null,
+        "startTime": "2024-05-04T08:15:53.215324",
+        "registrationNumber": "TUAbu Dhabi98676",
+        "vehicleRegistrationNumber": "9818",
+        "startTotalizer": "123",
+        "odometerReading": "123456789123",
+        "unitOfMeasure": "litre",
+        "isReceiptRequired": true
+    };
+
+    dispenser.execute(dispenser.printReceipt, dispenser.processCommand, printObj).then((totalizer) => {
         dispenser.disconnect(() => {
             console.log("Disconnected");
         });
