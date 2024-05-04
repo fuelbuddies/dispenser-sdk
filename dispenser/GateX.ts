a6af4c5137a796b9b51215730f7e94ef834f9732
Nitin Sharma <nitin@fuelbuddy.in>
2024-05-04 22:15:27 +0400

    NO_T fixing release.
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- dispenser/GateX.ts
+++ dispenser/GateX.ts
@@ -224,11 +224,8 @@ export class GateX extends ModBusDispenser {
             }
             printArr.push('0A');
             printArr.push(this.str2hex(this.rightAlignValue("GROSS VOLUME", printObj?.unitOfMeasure, printWidth)));
-            printArr.push('0A');
-            printArr.push('0A');
+            printArr.push('0A0A1D564100');
         }
-
-        printArr.push('1D564100');
 
         printArr.push(this.str2hex(this.centerAlignValue("****  PRINT COPY  ****", printWidth)));
         printArr.push('0A');
@@ -257,8 +254,8 @@ export class GateX extends ModBusDispenser {
         printArr.push('0A');
         printArr.push(this.str2hex(this.rightAlignValue("GROSS VOLUME", printObj?.unitOfMeasure, printWidth)));
 
-        this.debugLog("printReceipt", `${printArr.join('0A')}0A0A0A1D564100`);
-        return this.printOrder(`${printArr.join('0A')}0A0A0A1D564200`);
+        this.debugLog("printReceipt", `${printArr.join('0A')}0A0A1D564200`);
+        return this.printOrder(`${printArr.join('0A')}0A0A1D564200`);
     }
 
     printOrder(printText: string): boolean {
