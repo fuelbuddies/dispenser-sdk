a7c242bdd36f71b1e6f5978d1a77b996cf6de656
Nitin Sharma <nitin@fuelbuddy.in>
2024-05-11 16:24:12 +0400

    NO_T fixing release.
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- dispenser/GateX.ts
+++ dispenser/GateX.ts
@@ -29,8 +29,7 @@ export class GateX extends ModBusDispenser {
         return (await this.executeShellScriptAndCheck('scripts/GateX/status.sh')) ? "true" : "false";
     }
 
-    processTotalizerRes(data: any): TotalizerResponse {
-        const pulse = this.hexToDecLittleEndian(data.buffer.toString('hex'));
+    processTotalizerRes(pulse: any): TotalizerResponse {
         console.log("pulse", pulse);
         if(!this.kFactor || this.kFactor < 0) {
             console.warn('K-Factor not set for this dispenser, you might get wrong totalizer value');
