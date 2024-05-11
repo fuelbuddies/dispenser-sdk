e77839b4d84dad5d7c4af6e396b425518feb5f79
Nitin Sharma <nitin@fuelbuddy.in>
2024-05-11 18:48:19 +0400

    NO_T testing write
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

---
+++ .bin/test-write.ts
@@ -0,0 +1,28 @@
+import ModbusRTU from "modbus-serial";
+
+const timeout = 1000;
+const baudRate = 9600;
+const address = '/dev/ttyUSB1';
+const deviceId = 1;
+
+const overflowRegister = 8;
+const overflowCount = 0;
+
+async function run() {
+    const client = new ModbusRTU();
+    client.setID(deviceId);
+    client.setTimeout(timeout);
+    await client.connectRTUBuffered(address, { baudRate: baudRate });
+    console.log("Connected to Seneca");
+    await client.readHoldingRegisters(overflowRegister, 1).then((data) => {
+        console.log("Overflow Register: ", data.buffer.readUInt16BE(0));
+    });
+    const writeRegister = await client.writeRegister(this.overflowRegister, ++this.overflowCount);
+    console.log("writeRegister" ,writeRegister);
+    console.log("Incremented Overflow Register");
+    await client.readHoldingRegisters(overflowRegister, 1).then((data) => {
+        console.log("Overflow Register: ", data.buffer.readBigInt64BE(0));
+    });
+}
+
+run();
