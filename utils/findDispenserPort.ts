4b1b7e31b1e83a67b0c0f557ff46a7784242ecdc
Nitin Sharma <nitin@fuelbuddy.in>
2024-05-18 17:28:59 +0400

    NO_T fixing relesae.
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- utils/findDispenserPort.ts
+++ utils/findDispenserPort.ts
@@ -1,4 +1,5 @@
 import {SerialPort} from 'serialport';
+import { debugLog } from './debugLog';
 
 // Define the hardware ID and attribute ID you want to search for
 export const hardwareId = '0403';
@@ -7,6 +8,7 @@ export const attributeId = '6001';
 // Function to find port based on hardware ID and attribute ID
 export async function findDispenserPort(hardwareId: string, attributeId: string) {
     try {
+        debugLog(`Finding port with hardware ID: ${hardwareId}, and attribute ID: ${attributeId}`, arguments);
         const foundPort = (await SerialPort.list()).find(port => {
             return port.vendorId === hardwareId && port.productId === attributeId;
         });
