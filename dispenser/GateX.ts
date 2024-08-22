1051c00c8fc0d637e0e209da6f751ea3c78a77ca
Nitin Sharma <109578744+nitinfb@users.noreply.github.com>
2024-08-22 11:15:12 +0000

    NO_T bump and fix totalizer

--- dispenser/GateX.ts
+++ dispenser/GateX.ts
@@ -48,9 +48,9 @@ export class GateX extends ModBusDispenser {
             timestamp: (new Date()).getTime()
         } as TotalizerResponse;
 
-        if(!this.startTotalizer) {
-            this.startTotalizer = totalizer;
-        }
+        // if(!this.startTotalizer) {
+        //     this.startTotalizer = totalizer;
+        // }
         debugLog("totalizer: %o", totalizer);
         return totalizer;
     }
@@ -131,7 +131,8 @@ export class GateX extends ModBusDispenser {
     async authorizeSale() {
         try {
             if(!this.startTotalizer) {
-                await this.processTotalizerRes(await this.totalizer()); //This will initialize startTotalizer.
+                const totalizer = await this.processTotalizerRes(await this.totalizer()); //This will initialize startTotalizer.
+                this.startTotalizer = totalizer;
             }
 
             if(!this.startTotalizer) {
