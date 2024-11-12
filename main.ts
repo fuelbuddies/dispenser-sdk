8db7b1db4c2f3424a076b14748754a35e1d36b79
Shreesh Katyayan <shreesh.katyayan@fuelbuddy.in>
2024-11-12 15:30:31 +0400

    NO_T some progress

--- main.ts
+++ main.ts
@@ -8,9 +8,10 @@ import { Seneca } from "./dispenser/workflows/GateX";
 import { delay } from "./utils/delay";
 import debug from 'debug';
 import { findPrinterPort } from "./utils/findPrinterPort";
+import { TCS3000 } from "./dispenser/TCS3000";
 
 const debugLog = debug('dispenser:main');
-export { IDispenser, IRfid, RfidOptions, DispenserOptions, getConfigFromEnv, getRFIDConfigFromEnv};
+export { IDispenser, IRfid, RfidOptions, DispenserOptions, getConfigFromEnv, getRFIDConfigFromEnv };
 
 /* Factory for creating dispenser objects */
 export async function createDispenser(options: DispenserOptions): Promise<IDispenser> {
@@ -20,21 +21,25 @@ export async function createDispenser(options: DispenserOptions): Promise<IDispe
             const DispenserType = await import('./dispenser/Tokhiem');
             const tokhiemUsbPath = await findDispenserPort(hardwareId, attributeId);
             debugLog('Dispenser found at: %o', tokhiemUsbPath);
-            return new DispenserType.Tokhiem(new SerialPort({path: tokhiemUsbPath, baudRate: baudRate }), options);
+            return new DispenserType.Tokhiem(new SerialPort({ path: tokhiemUsbPath, baudRate: baudRate }), options);
         case 'IsoilVegaTVersion10':
             const IsoilVegaTVersion10 = await import('./dispenser/IsoilVegaTVersion10');
             const ISoilUsbPath = await findDispenserPort(hardwareId, attributeId);
             debugLog('Dispenser found at: %o', ISoilUsbPath);
-            return new IsoilVegaTVersion10.IsoilVegaTVersion10(new SerialPort({path: ISoilUsbPath, baudRate: baudRate }), options);
+            return new IsoilVegaTVersion10.IsoilVegaTVersion10(new SerialPort({ path: ISoilUsbPath, baudRate: baudRate }), options);
+        case 'TCS3000':
+            const TCSUsbPath = await findDispenserPort(hardwareId, attributeId);
+            debugLog('Dispenser found at: %o', TCSUsbPath);
+            return new TCS3000(new SerialPort({ path: TCSUsbPath, baudRate: 9600 }));
         case 'GateX':
             const GateX = await import('./dispenser/GateX');
             const serialPort = new Seneca(options);
             serialPort.address = await findDispenserPort(hardwareId, attributeId);
             debugLog('Dispenser found at: %o', serialPort.address);
-            if(!printer) throw new Error('Printer is required for GateX dispenser');
+            if (!printer) throw new Error('Printer is required for GateX dispenser');
             const GateXPrinterPath = await findPrinterPort(printer.hardwareId, printer.attributeId);
             debugLog('Printer found at: %o', GateXPrinterPath);
-            const printerPort = new SerialPort({path: GateXPrinterPath, baudRate: printer.baudRate || 9600});
+            const printerPort = new SerialPort({ path: GateXPrinterPath, baudRate: printer.baudRate || 9600 });
             const gatex = new GateX.GateX(serialPort, printerPort, options);
             await delay(5000);
             return gatex;
@@ -48,7 +53,7 @@ export async function createRfid(options: RfidOptions): Promise<IRfid> {
     const { rfidType, hardwareId, attributeId, baudRate = 19200 } = options;
     switch (rfidType) {
         case 'PETROPOINTHECTRONICS':
-            const serialPort = new SerialPort({path: await findRfidPort(hardwareId, attributeId), baudRate: baudRate });
+            const serialPort = new SerialPort({ path: await findRfidPort(hardwareId, attributeId), baudRate: baudRate });
             const RfidPetropoint = await import('./rfid/RfidPetropoint');
             return new RfidPetropoint.RfidPetropoint(serialPort);
         default:
