7789c913b81b996d46fc8b183eb151d1e7db81ea
Nitin Sharma <nitin@fuelbuddy.in>
2024-11-23 22:36:23 +0530

    NO_T added clear sale
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

---
+++ .bin/clear-sale.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env ts-node
+
+import { createDispenser } from '../main';
+import debug from 'debug';
+import {getConfigFromEnv} from '../utils/envParser';
+
+const debugLog = debug('dispenser:main');
+const configuration = getConfigFromEnv();
+debugLog('Configuration: %O', configuration);
+createDispenser(configuration).then((dispenser) => {
+    // console.log(dispenser);
+    dispenser.execute(dispenser.clearSale, dispenser.processCommand).then((totalizer) => {
+        dispenser.disconnect(() => {
+            console.log("Disconnected");
+        });
+        console.log(totalizer);
+    });
+});
