9dbdf688938c74a9307445a32a278dfe43df337f
Nitin Sharma <nitin@fuelbuddy.in>
2024-12-21 13:07:43 +0530

    NO_T trying isSaleCloseable
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- dispenser/VeederEmr4.ts
+++ dispenser/VeederEmr4.ts
@@ -418,10 +418,10 @@ export class VeederEmr4 extends BaseDispenser {
   isSaleCloseable(res: string) {
     const dispenserStatus = this.processStatus(res);
     debugLog("isSaleCloseable: %s", dispenserStatus);
-    if (dispenserStatus.get("Net Preset Is Active") || dispenserStatus.get("Delivery Error")) {
+    // if (dispenserStatus.get("Net Preset Is Active") || dispenserStatus.get("Delivery Error")) {
       return true;
-    }
-    return false;
+    // }
+    // return false;
   }
 
   /**
