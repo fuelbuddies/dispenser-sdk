cafdbef8b3d2079e7d732ff076cc624af3ebe8f1
Nitin Sharma <nitin@fuelbuddy.in>
2024-12-21 17:38:17 +0530

    NO_T fixing release.
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- dispenser/base/scripts/EMR4/startpump.sh
+++ dispenser/base/scripts/EMR4/startpump.sh
@@ -6,7 +6,20 @@ SCRIPT_PATH=$(dirname "$0")
 
 pumpPin=${VITE_MAIN_DISPENSER_PUMP_PIN:-22}
 
+# function for checking pump status
+function pumpstatus() {
+    # Run the command and capture its output
+    pin_state=$(pinctrl get ${pumpPin})
+
+    # Check if the pin state contains "hi"
+    if [[ $pin_state == *"$1"* ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
 pinctrl set ${pumpPin} op dl
 
 # Run the command and capture its output
-${SCRIPT_PATH}/pumpstatus.sh lo
+pumpstatus lo
