7e6e4adacbad93e16c30de5a105f47685254ce63
Shreesh Katyayan <shreesh.katyayan@fuelbuddy.in>
2024-12-29 00:44:14 +0400

    NO_T fixes in test script

--- .bin/start-dispensing.ts
+++ .bin/start-dispensing.ts
@@ -3,6 +3,7 @@
 import { createDispenser } from '../main';
 import debug from 'debug';
 import { getConfigFromEnv } from '../utils/envParser';
+import { delay } from '../utils/delay';
 
 const debugLog = debug('dispenser:main');
 
@@ -13,13 +14,17 @@ createDispenser(configuration).then((dispenser) => {
 	try {
 		dispenser.execute(dispenser.setPreset, dispenser.processCommand, 1000).then((presetStatus) => {
 			debugLog('presetStatus: %o', presetStatus);
-			dispenser.execute(dispenser.pumpStart, dispenser.processCommand).then((pumpStatus) => {
-				debugLog('pumpStatus: %o', pumpStatus);
-				dispenser.execute(dispenser.authorizeSale, dispenser.processCommand).then((totalizer) => {
-					dispenser.disconnect(() => {
-						debugLog('Disconnected');
+			delay(500).then(() => {
+				dispenser.execute(dispenser.pumpStart, dispenser.processCommand).then((pumpStatus) => {
+					debugLog('pumpStatus: %o', pumpStatus);
+					delay(500).then(() => {
+						dispenser.execute(dispenser.authorizeSale, dispenser.processCommand).then((totalizer) => {
+							dispenser.disconnect(() => {
+								debugLog('Disconnected');
+							});
+							debugLog(totalizer);
+						});
 					});
-					debugLog(totalizer);
 				});
 			});
 		});
