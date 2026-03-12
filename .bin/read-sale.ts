2f5a5a43cb190b98fc1c493f1fe7a617a1ff6e50
abhi-fuelbuddy <abhishek.ramola@fuelbuddy.in>
2026-03-12 18:16:51 +0530

    refactor(read-sale): remove commented code block

--- .bin/read-sale.ts
+++ .bin/read-sale.ts
@@ -9,18 +9,6 @@ const configuration = getConfigFromEnv();
 debugLog('Configuration: %O', configuration);
 
 createDispenser(configuration).then((dispenser) => {
-	// // Neogi uses LT command which returns full transaction details
-	// // Other dispensers may return order completion status
-	// const isNeogi = dispenser.checkType?.() === 'NEOGI';
-	// const processor = isNeogi
-	// 	? dispenser.processReadSale
-	// 	: dispenser.isOrderComplete;
-
-	// debugLog('Dispenser type: %s, Using processor: %s',
-	// 	dispenser.checkType?.() || 'Unknown',
-	// 	isNeogi ? 'processReadSale' : 'isOrderComplete'
-	// );
-
 	dispenser.execute(dispenser.readSale, dispenser.isOrderComplete).then((response) => {
 		dispenser.disconnect(() => {
 			console.log('Disconnected');
